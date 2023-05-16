0b6ff39cb49a9d0b03214a0052a19beab146ced5
MiguelPiffer <miguel-piffer1@hotmail.com>
2023-05-16 15:39:52 -0300

    threshold

--- yolov8_Miguel_Piffer/arquivo_para_video.py
+++ yolov8_Miguel_Piffer/arquivo_para_video.py
@@ -14,7 +14,7 @@ ret, frame = cap.read()
 H, W, _ = frame.shape
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', '')
+model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', '/Users/miguelpiffer/Desktop/estudos/projeto integrador/runs/detect/train2/weights/best.pt')
 
 # Load a model
 model = YOLO(model_path)  # load a custom model
